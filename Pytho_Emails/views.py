a8a6aa9d78bedbb986dd0842df617cc1803b7a10
mortada gzar <mortadagzar@gmail.com>
2018-10-03 23:09:11 -0700

    dine

--- Pytho_Emails/views.py
+++ Pytho_Emails/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import myUserForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def index (request):
        return render(request,'myApp/index.html')
+
+def thanks (request):
+       return render(request,'myApp/thanks.html')       
+
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('thanks')
+    else:
+        form = UserCreationForm()
+    return render(request, 'myApp/index.html', {'form': form})    
--- Python_EmailsViews/urls.py
+++ Python_EmailsViews/urls.py
@@ -6,5 +6,8 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.index),
+    url(r'^$', views.index,name="index"),
+    path('signup', views.signup,name="signup"),
+    path('thanks', views.thanks,name="thanks"),
+   
 ]
