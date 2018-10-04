19c842d12f6e3303afd040c24afd5dbb0a300f5c
mortada gzar <mortadagzar@gmail.com>
2018-10-03 18:26:12 -0700

    ,ooo

--- Python_EmailsViews/settings.py
+++ Python_EmailsViews/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+#hello ju
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -58,7 +58,7 @@ TEMPLATES = [
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [
+            'context_processors': [ 
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
